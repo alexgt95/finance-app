0fa402daf78fa001d7bf72a493ac27582db683e4
alexgt95 <ale.g.trujillo2005@gmail.com>
2024-11-12 20:33:35 -0600

    Post API 19

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { QueryProvider } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,8 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <QueryProvider>
-            {children}
+            <SheetProvider />
+              {children}
           </QueryProvider>
         </body>
       </html>
