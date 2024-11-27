e63e2585dbeed16dc3f0feed226d80513aece566
alexgt95 <ale.g.trujillo2005@gmail.com>
2024-11-26 18:00:48 -0600

    Data Table 20

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,29 +1,9 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Plus } from "lucide-react";
-
-const AccountsPage = () => {
-
+const MainPage = () => {
   return (
-      <div>
-          <Card className="border-none drop-shadow-sm">
-              <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-                <CardTitle className="text-xl line-clamp-1">
-                  Accounts page
-                </CardTitle>
-                <Button size="sm">
-                  <Plus className="size-4 mr-2" />
-                  Add new
-                </Button>
-              </CardHeader>
-          </Card>
-      </div>
+    <div>
+      Main Page
+    </div>
   );
 };
 
-export default AccountsPage;
+export default MainPage;
